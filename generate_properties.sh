9af69717682fb11a815717eb02a766c2e5e5ecd1
Nicolas Marlier <nmarlier@gmail.com>
2015-04-02 14:17:49 +0200

    IW"

--- generate_properties.sh
+++ generate_properties.sh
@@ -1,10 +1,11 @@
 rm -f -r davmail.properties
 while read line
 do
-if [ "$line" == "davmail.caldavPort=" ]
+if [ "$line" = "davmail.caldavPort=" ]
 then
 echo "davmail.caldavPort=$PORT\n" >> "davmail.properties"
 else
 echo "$line\n" >> "davmail.properties"
 fi
 done <davmail.default.properties
+echo "Port is '$PORT'."
